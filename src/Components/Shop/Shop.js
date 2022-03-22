baf9bb862a24b5f5df0384aa3459c60d1184cce4
mahedi <mahedianik4@gmail.com>
2022-03-22 10:28:11 +0600

    shipping,and tax total added

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -13,7 +13,27 @@ const Shop = () => {
     const cartHandler =(product)=>{
         const newCart =[...cart,product];
         setCart(newCart)
+        totalPrice(product.price)
     };
+    const [price,setPrice]=useState([0])
+    const [shipping,setShipping]= useState([20])
+    const [tax,setTax] = useState([0])
+    const [total,setTotal] = useState([0])
+    const totalPrice =(prices)=>{
+       const newPrice = parseInt(price +prices);
+       setPrice(newPrice)
+       shippingCost()
+       totalTax(price)
+    };
+    
+    const shippingCost=()=>{
+        setShipping(shipping)
+    }
+    const totalTax =(price)=>{
+        const newTax = price*(5/100)
+        setTax(newTax)
+    };
+    
     return (
         <div className='flex flex-raw  relative flex-wrap '>
             <div className='basis-4/5 grid grid-cols-3 
@@ -29,9 +49,17 @@ const Shop = () => {
             <div className=' fixed top-30 right-5 
             bg-orange-200  h-3/4  w-44 rounded-lg'>
                 <h3 className='text-center text-xl
-                 text-black '>Order Summary</h3>
-                <h2 className='text-center text-xl
-                 text-black '>select items:{cart.length}</h2>
+                 text-black font-bold'>Order Summary</h3>
+                 <div className='text-left p-4 font-semibold leading-8'>
+                     <p><span>selected items: {cart.length}</span></p>
+                     <p><span>Total Price: ${price}:</span></p>
+                     <p><span>Shipping Cost: ${shipping}</span></p>
+                     <p><span>Tax:${tax}:</span></p>
+                     <p className='mt-5 font-bold'>Grand Total:${total}:</p>
+                     <button className="h-8 px-7 text-white transition-colors duration-150 bg-red-500 rounded-full focus:shadow-outline hover:bg-red-600 mb-5 mt-5">Clear Cart </button>
+                     
+                     <button className="h-8 px-4 text-white transition-colors duration-150 bg-red-500 rounded-full focus:shadow-outline hover:bg-red-600">Review Order</button>
+                 </div>
             </div>
 
         </div>
