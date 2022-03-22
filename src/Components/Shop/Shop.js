d720a6fd91ba77184875e0c380354cd8f6d65120
mahedi <mahedianik4@gmail.com>
2022-03-22 17:58:56 +0600

    project done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
-
-
 const Shop = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
@@ -9,31 +7,30 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
-    const [cart, setCart] =useState([])
-    const cartHandler =(product)=>{
-        const newCart =[...cart,product];
+    const [cart, setCart] = useState([])
+    const cartHandler = (product) => {
+        const newCart = [...cart, product];
         setCart(newCart)
         totalPrice(product.price)
     };
-    const [price,setPrice]=useState([0])
-    const [shipping,setShipping]= useState([20])
-    const [tax,setTax] = useState([0])
-    const [total,setTotal] = useState([0])
-    const totalPrice =(prices)=>{
-       const newPrice = parseInt(price +prices);
-       setPrice(newPrice)
-       shippingCost()
-       totalTax(price)
+    const [price, setPrice] = useState([0])
+    const [shipping, setShipping] = useState([0])
+    const [tax, setTax] = useState([0])
+    const totalPrice = (prices) => {
+        const newPrice = parseInt(price + prices);
+        setPrice(newPrice)
+        shippingCost()
+        totalTax(price)
     };
-    
-    const shippingCost=()=>{
-        setShipping(shipping)
+
+    const shippingCost = () => {
+        setShipping(parseInt(20))
     }
-    const totalTax =(price)=>{
-        const newTax = price*(5/100)
+    const totalTax = (price) => {
+        const newTax = Math.round(price * (5 / 100))
         setTax(newTax)
     };
-    
+
     return (
         <div className='flex flex-raw  relative flex-wrap '>
             <div className='basis-4/5 grid grid-cols-3 
@@ -50,16 +47,16 @@ const Shop = () => {
             bg-orange-200  h-3/4  w-44 rounded-lg'>
                 <h3 className='text-center text-xl
                  text-black font-bold'>Order Summary</h3>
-                 <div className='text-left p-4 font-semibold leading-8'>
-                     <p><span>selected items: {cart.length}</span></p>
-                     <p><span>Total Price: ${price}:</span></p>
-                     <p><span>Shipping Cost: ${shipping}</span></p>
-                     <p><span>Tax:${tax}:</span></p>
-                     <p className='mt-5 font-bold'>Grand Total:${total}:</p>
-                     <button className="h-8 px-7 text-white transition-colors duration-150 bg-red-500 rounded-full focus:shadow-outline hover:bg-red-600 mb-5 mt-5">Clear Cart </button>
-                     
-                     <button className="h-8 px-4 text-white transition-colors duration-150 bg-red-500 rounded-full focus:shadow-outline hover:bg-red-600">Review Order</button>
-                 </div>
+                <div className='text-left p-4 font-semibold leading-8'>
+                    <p><span>selected items: {cart.length}</span></p>
+                    <p><span>Total Price: ${price}:</span></p>
+                    <p><span>Shipping Cost: ${shipping}</span></p>
+                    <p><span>Tax:${tax}:</span></p>
+                    <p className='mt-5 font-bold'>Grand Total:$:</p>
+                    <button className="h-8 px-7 text-white transition-colors duration-150 bg-red-500 rounded-full focus:shadow-outline hover:bg-red-600 mb-5 mt-5">Clear Cart </button>
+
+                    <button className="h-8 px-4 text-white transition-colors duration-150 bg-red-500 rounded-full focus:shadow-outline hover:bg-red-600">Review Order</button>
+                </div>
             </div>
 
         </div>
